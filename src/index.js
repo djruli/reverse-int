e4f3fb4c8d9f9b7c58f90d2f6edb9abcfd9e4146
Roman Popov <djrulik@gmail.com>
2023-08-02 03:39:05 +0300

    init: update commit

--- src/index.js
+++ src/index.js
@@ -21,6 +21,26 @@ module.exports = function reverse (n) {
     let result = array.toString().replace(/[,]/g, '');
     let parsed = parseInt(result, 10);
     return parsed;
+    /* at console
+    function reverse (n) {
+    let num  = Math.abs(n);
+    let array = [];
+    while(num>0){
+    array.push(num%10);
+    console.log(num%10);
+    console.log(array);
+    console.log("before", num);
+    num=num/10|0;
+    console.log(num);
+    }
+    console.log(array);
+    let result = array.toString().replace(/[,]/g, '');
+    console.log(result);
+    let parsed = parseInt(result, 10);
+    console.log(parsed);
+    return parsed;
+}
+    reverse(-1234567890);*/
 
 
 }
